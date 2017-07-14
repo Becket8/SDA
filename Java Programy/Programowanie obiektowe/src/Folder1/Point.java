f0701d543cb916398fbe6f6473c15ff4bc758ca6
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-07-14 15:04:37 +0200

    no message

--- Java Programy/Programowanie obiektowe/src/Folder1/Point.java
+++ Java Programy/Programowanie obiektowe/src/Folder1/Point.java
@@ -1,18 +1,15 @@
 package Folder1;
 
 public class Point {
-    public double x;
-    public double y;
-    public double wynik;
-    public Point(){
+    public float x;
+    public float y;
+
+    public Point() {
 
     }
-    public double DistanceFromOrigin(double z){
-        z= Math.sqrt(x*x+y*y);
-        wynik = z;
 
-        System.out.println("Odleglosc to: "+ this.wynik);
-        return wynik;
+    public double DistanceFromOrigin() {
+        return Math.sqrt(x * x + y * y);
+
     }
-
 }
--- Java Programy/Programowanie obiektowe/src/Main1.java
+++ Java Programy/Programowanie obiektowe/src/Main1.java
@@ -4,11 +4,11 @@ public class Main1 {
     public static void main(String[] args) {
         Point P1 = new Point();
         Point P2 = new Point();
-        P1.x = 1.0;
-        P1.y = 1.0;
-        P2.x = 5.0;
-        P2.y = 5.0;
-        P1.DistanceFromOrigin();
+        P1.x = (float) 1.0;
+        P1.y = (float) 1.0;
+        P2.x = (float) 5.0;
+        P2.y = (float) 5.0;
+        System.out.println("Odlegosc to: " + P1.DistanceFromOrigin());
 
     }
 }
