3b7d5a560c4f9147c1c816766b1487bff81b1deb
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-07-16 21:00:19 +0200

    no message

--- Java Programy/Programowanie obiektowe/src/Klasy/Firma.java
+++ Java Programy/Programowanie obiektowe/src/Klasy/Firma.java
@@ -1,11 +1,24 @@
 package Klasy;
 
-public class Firma {
+public class Firma extends Pracownik2 {
     private Pracownik2[] pracownicy;
 
-    public Firma(){
-        pracownicy = new pracownicy[10];
+
+    public Firma() {
+       pracownicy = new Pracownik2[10];
     }
 
-    //napisac metode ktora wyswietli wszystkich pracownikow
-}
+    public String[] SpisPracownikow(String[] pracownicy) {
+        for (int i=0;i<pracownicy.length;i++){
+            String dane  = this.getName();
+            pracownicy[i] = dane;
+            System.out.println(pracownicy[i]);
+        }
+        return pracownicy;
+        }
+
+    }
+
+
+
+//napisac metode ktora wyswietli wszystkich pracownikow
--- Java Programy/Programowanie obiektowe/src/Object_Firma.java
+++ Java Programy/Programowanie obiektowe/src/Object_Firma.java
@@ -1,5 +1,23 @@
-/**
- * Created by Sowskin on 16.07.2017.
- */
+import Klasy.Firma;
+
+import java.util.Scanner;
+
 public class Object_Firma {
+    public static void main(String[] args) {
+        System.out.println("Podaj dane pracownika: ");
+        Scanner sc = new Scanner(System.in);
+        Firma pracownik1 = new Firma();
+        System.out.println("Poaj imie: ");
+        pracownik1.setName(sc.nextLine());
+        System.out.println("Poaj nazwisko: ");
+        pracownik1.setSurname(sc.nextLine());
+        System.out.println("Poaj wiek: ");
+        pracownik1.setAge(sc.nextInt());
+        System.out.println("Poaj pensje: ");
+        pracownik1.setSalary(sc.nextInt());
+
+        System.out.println("Dane pracowników: " + "\n"+pracownik1.SpisPracownikow());
+
+
+    }
 }
