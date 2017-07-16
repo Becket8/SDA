f7b5f70b8165e5dd89a8ca48e4504c66fe593e4b
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-07-16 18:20:52 +0200

    no message

---
+++ Java Programy/Programowanie obiektowe/src/Klasy/Firma.java
@@ -0,0 +1,11 @@
+package Klasy;
+
+public class Firma {
+    private Pracownik2[] pracownicy;
+
+    public Firma(){
+        pracownicy = new pracownicy[10];
+    }
+
+    //napisac metode ktora wyswietli wszystkich pracownikow
+}
---
+++ Java Programy/Programowanie obiektowe/src/Klasy/Pracownik2.java
@@ -0,0 +1,58 @@
+package Klasy;
+
+public class Pracownik2 {
+
+        private String name;
+        private String surname;
+        private int age;
+        private int salary;
+
+
+        public Pracownik2() {
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+
+        public void setAge(int age) {
+            if (age < 0) {
+                System.out.println("Podano ujemny wiek");
+
+            } else
+                this.age = age;
+        }
+
+
+        public int getSalary() {
+            return salary;
+        }
+
+        public void setSalary(int salary) {
+            if (salary < 0) {
+                System.out.println("Pensja nie moze byc ujemna");
+            } else {
+                this.salary = salary;
+            }
+
+        }
+    }
+}
