f03d183d96f7009c3b3acd3aba6287cf77c901ce
Becket8 <filip.sosnowski88@gmail.com>
2017-07-18 21:00:10 +0200

    no message

--- Java Programy/Programowanie obiektowe/src/Klasy/Mathematic/Matrix.java
+++ Java Programy/Programowanie obiektowe/src/Klasy/Mathematic/Matrix.java
@@ -6,12 +6,38 @@ public class Matrix {
     private int[][] matrix;
     private int m;
     private int n;
+
     public Matrix()
 
     {
         m = 3;
         n = 3;
         matrix = new int[m][n];
+    }
+
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int getM() {
+        return m;
+    }
+
+    public void setM(int m) {
+        this.m = m;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
     }
 
     public void initializeMatrix() {
@@ -30,7 +56,7 @@ public class Matrix {
     public void printMatrix() {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print( matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
@@ -38,11 +64,17 @@ public class Matrix {
 
     }
 
-    public void addMatrix(int[][] m) {
+    public Matrix addMatrix(Matrix matrix2) {
+        Matrix summary = new Matrix();
 
-    }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                summary.getMatrix()[i][j] = this.matrix[i][j] + matrix2.getMatrix()[i][j];
+            }
 
-    public void addMatrixToMatrix() {
+        }
+        return summary;
 
     }
 }
+
