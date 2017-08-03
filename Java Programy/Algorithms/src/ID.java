85c1eb0ffab650f0425eddc9f121987357738be6
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-08-03 16:53:20 +0200

    no message

--- Java Programy/Algorithms/src/ID.java
+++ Java Programy/Algorithms/src/ID.java
@@ -3,42 +3,34 @@ import java.util.Scanner;
 public class ID {
 
     public static void main(String[] args) {
-        boolean poprawnyNumer = false;
-        String dowod;// = "ABC123456";
+        String ID;
 
-        //while (poprawnyNumer) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj serie i numer dowodu osobistego: ");
-        dowod = scanner.next();
-            /*if (dowod.length()==10){
-                poprawnyNumer=true;
-            } else {
-                System.out.println("Nie poprawna seria i numer dowodu. \n");
-            }
+        ID = scanner.next();
 
-        }*/
-        int[] tabDowod = new int[9];
-        int[] test = new int[]{7,1,3,7,1,3,7,3};
-        int[] wynDowod = new int[9];
-        int suma = 0;
+        int[] arrID = new int[9];
+        int[] test = new int[]{7, 3, 1, 0, 7, 3, 1, 7, 3};
+        int[] sumID = new int[9];
+        int summary = 0;
 
         for (int i = 0; i < 9; i++) {
             if (i < 3) {
-                tabDowod[i] = (int) dowod.charAt(i) - 55;
+                arrID[i] = (int) ID.charAt(i) - 55;
             } else {
-                tabDowod[i] = (int) dowod.charAt(i) - 48;
+                arrID[i] = (int) ID.charAt(i) - 48;
             }
         }
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 9; i++) {
             if (i == 3) {
             } else {
-                wynDowod[i] = tabDowod[i] * test[i];
-                suma += wynDowod[i];
+                sumID[i] = arrID[i] * test[i];
+                summary += sumID[i];
             }
         }
 
-        System.out.println(Math.floorMod(suma, 10));
+        System.out.println(Math.floorMod(summary, 10));
 
     }
 }
