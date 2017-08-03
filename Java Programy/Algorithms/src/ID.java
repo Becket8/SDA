fbb7aace0db2af853e1c1cb8a1afb11282457045
SOWSKIN\Sowskin <filip.sosnowski88@gmail.com>
2017-08-03 16:37:36 +0200

    no message

--- Java Programy/Algorithms/src/ID.java
+++ Java Programy/Algorithms/src/ID.java
@@ -1,17 +1,44 @@
+import java.util.Scanner;
 
 public class ID {
+
     public static void main(String[] args) {
+        boolean poprawnyNumer = false;
+        String dowod;// = "ABC123456";
 
-
-        String wynik = "";
-        for (int i = 0; i < ; i++) {
-            char znak = napis.charAt(i);
-            if (znak >= 97 && znak <= 122) {
-                znak -= 32;
+        //while (poprawnyNumer) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj serie i numer dowodu osobistego: ");
+        dowod = scanner.next();
+            /*if (dowod.length()==10){
+                poprawnyNumer=true;
+            } else {
+                System.out.println("Nie poprawna seria i numer dowodu. \n");
             }
-            wynik += znak;
 
+        }*/
+        int[] tabDowod = new int[9];
+        int[] test = new int[]{7,1,3,7,1,3,7,3};
+        int[] wynDowod = new int[9];
+        int suma = 0;
+
+        for (int i = 0; i < 9; i++) {
+            if (i < 3) {
+                tabDowod[i] = (int) dowod.charAt(i) - 55;
+            } else {
+                tabDowod[i] = (int) dowod.charAt(i) - 48;
+            }
         }
-        System.out.println(wynik);
+
+        for (int i = 0; i < 8; i++) {
+            if (i == 3) {
+            } else {
+                wynDowod[i] = tabDowod[i] * test[i];
+                suma += wynDowod[i];
+            }
+        }
+
+        System.out.println(Math.floorMod(suma, 10));
+
     }
 }
