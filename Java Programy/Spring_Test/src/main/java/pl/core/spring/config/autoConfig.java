afc4b60c56b16e2796c4e3cfc4ac85b325b6a850
Filip Sosnowski <filip.sosnowski88@gmail.com>
2017-09-04 17:49:07 +0200

    no message

--- Java Programy/Spring_Test/src/main/java/pl/core/spring/config/autoConfig.java
+++ Java Programy/Spring_Test/src/main/java/pl/core/spring/config/autoConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration;
  * Created by Sowskin on 04.09.2017.
  */
 @Configuration
-@ComponentScan("pl.core.spring.implementation")
+@ComponentScan("pl.core.spring")
 public class autoConfig {
 }
--- Java Programy/Spring_Test/src/main/java/pl/core/spring/main/Main.java
+++ Java Programy/Spring_Test/src/main/java/pl/core/spring/main/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(autoConfig.class);
-        IOrder order = (IOrder)context.getBean("Order");
+        IOrder order = context.getBean(IOrder.class);
         order.printOrder();
     }
 }
