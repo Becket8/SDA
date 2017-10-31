652c6164c322139426ed19349053faff777a2842
Filip Sosnowski <filip.sosnowski88@gmail.com>
2017-10-31 17:46:45 +0100

    no message

--- Java Programy/Spring/src/main/java/com/sda/springjavapoz4/controller/HomeController.java
+++ Java Programy/Spring/src/main/java/com/sda/springjavapoz4/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.sda.springjavapoz4.controller;
 
-import com.sda.springjavapoz4.service.RandomGeneratorNumberService;
+import com.sda.springjavapoz4.service.RandomNumbersGeneratorService;
 import com.sda.springjavapoz4.service.SomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,68 +8,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.jws.WebParam;
-
 @Controller
 public class HomeController {
 
     @Autowired
     private SomeService someService;
 
-    @Autowired
-    private RandomGeneratorNumberService randomGeneratorNumberService;
-    @Qualifier("bigNumber")
-    @Autowired
-    private RandomGeneratorNumberService bigRandomGeneratorNumberService;
-    @Qualifier("smallNumber")
-    @Autowired
-    private RandomGeneratorNumberService smallRandomGeneratorNumberService;
-
     @Qualifier("aboutService")
     @Autowired
     private SomeService myService;
 
+    @Qualifier("smallNumbersGenerator")
+    @Autowired
+    private RandomNumbersGeneratorService randomNumbersGeneratorService;
 
     public HomeController() {
-
+//        this.someService = new SomeService();
     }
 
     @GetMapping("/")
     public ModelAndView home() {
+        System.out.println(randomNumbersGeneratorService.generateNumber());
+        System.out.println(randomNumbersGeneratorService.generateNumber());
+        System.out.println(randomNumbersGeneratorService.generateNumber());
+        System.out.println(randomNumbersGeneratorService.generateNumber());
+        System.out.println(randomNumbersGeneratorService.generateNumber());
+        System.out.println(randomNumbersGeneratorService.generateNumber());
         someService.someAction();
-        bigRandomGeneratorNumberService.showNumber();
-        bigRandomGeneratorNumberService.showNumber();
         return new ModelAndView("home");
     }
 
     @GetMapping("/about")
     public ModelAndView about() {
-        smallRandomGeneratorNumberService.showNumber();
-        smallRandomGeneratorNumberService.showNumber();
-        smallRandomGeneratorNumberService.showNumber();
-        someService.someAction();
+        System.out.println(randomNumbersGeneratorService.generateNumber());
+        myService.someAction();
         return new ModelAndView("home");
     }
-
-    @GetMapping("/generate")
-    public ModelAndView generate() {
-
-        bigRandomGeneratorNumberService.showNumber();
-        return new ModelAndView("home");
-
-    }
-
-    @GetMapping("/bigGenerate")
-    public ModelAndView bigGenerate(){
-        smallRandomGeneratorNumberService.showNumber();
-        return new ModelAndView("home");
-    }
-
-    @GetMapping("/smallGenerate")
-    public ModelAndView smallGenerate(){
-        randomGeneratorNumberService.showNumber();
-        return new ModelAndView("home");
-    }
-
-
 }
